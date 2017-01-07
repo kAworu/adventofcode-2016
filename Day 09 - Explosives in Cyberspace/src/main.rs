071c402e5001265e6dfc2efebf9c26062cf35923
Alexandre Perrin <alex@kaworu.ch>
2017-01-07 23:50:28 +0100

    Day 9: Explosives in Cyberspace (part2)

--- Day 09 - Explosives in Cyberspace/src/main.rs
+++ Day 09 - Explosives in Cyberspace/src/main.rs
@@ -2,46 +2,66 @@
 extern crate nom;
 
 mod explosives_in_cyberspace {
-    /// Represents a string repeated one or more times.
+    /// Represents a node from the `Ezip` "tree". Either an uncompressed chunk of data or a
+    /// sub-`Ezip` to be repeated.
     #[derive(Debug)]
-    struct ExpzipToken {
-        data: String,
-        repeat: usize,
+    enum EzipNode {
+        Uncompressed(String),
+        Compressed(usize, Ezip),
     }
 
-    impl ExpzipToken {
-        /// Create a new compressed, `repeat` is expected to be greater than one.
-        fn compressed(data: String, repeat: usize) -> ExpzipToken {
-            ExpzipToken { data: data, repeat: repeat }
-        }
-
-        /// Create a new uncompressed token.
-        fn uncompressed(data: String) -> ExpzipToken {
-            Self::compressed(data, 1)
-        }
-
-        /// Returns the uncompressed data length for this token.
+    impl EzipNode {
+        /// Returns the uncompressed data length for this node.
         fn uncompressed_len(&self) -> usize {
-            self.data.len() * self.repeat
+            match *self {
+                EzipNode::Uncompressed(ref s) => s.len(),
+                EzipNode::Compressed(repeat, ref children) => {
+                    repeat * children.uncompressed_len()
+                },
+            }
         }
     }
 
     /// Experimental data compression format found in the Easter Bunny HQ.
     #[derive(Debug)]
-    pub struct Expzip {
-        tokens: Vec<ExpzipToken>,
+    pub struct Ezip {
+        nodes: Vec<EzipNode>,
     }
 
-    impl Expzip {
+    impl Ezip {
+        /// Parse a string formated in the Experimental data compression format version 1.
+        // XXX: leaking nom stuff through the error, oh well.
+        pub fn parse_v1(s: &str) -> Result<Ezip, ::nom::IError> {
+                parsing::ezipv1(s).to_full_result()
+        }
+
+        /// Parse a string formated in the Experimental data compression format version 2.
+        // XXX: leaking nom stuff through the error, oh well.
+        pub fn parse_v2(s: &str) -> Result<Ezip, ::nom::IError> {
+                parsing::ezipv2(s).to_full_result()
+        }
+
         /// Returns the uncompressed data length of the file.
         pub fn uncompressed_len(&self) -> usize {
-            self.tokens.iter().map(|token| token.uncompressed_len()).sum()
+            self.nodes.iter().map(|token| token.uncompressed_len()).sum()
+        }
+
+        /// Build a new `Ezip` containing the given nodes.
+        fn build(nodes: Vec<EzipNode>) -> Ezip {
+            Ezip { nodes: nodes }
+        }
+
+        /// Build a new `Ezip` containing only one uncompressed node.
+        fn build_uncompressed(data: &str) -> Ezip {
+            Ezip {
+                nodes: vec![EzipNode::Uncompressed(data.to_string())],
+            }
         }
     }
 
-    // the parsing module impl Str for Expzip using nom.
+    // the Ezip parsing stuff using nom.
     mod parsing {
-        use explosives_in_cyberspace::{ExpzipToken, Expzip};
+        use explosives_in_cyberspace::{EzipNode, Ezip};
         use nom::{self, digit};
         use std::str::{self, FromStr};
 
@@ -53,44 +73,69 @@ mod explosives_in_cyberspace {
             )
         );
 
-        // parse a compressed marker and its associated data (i.e. something like "(3x3)XYZ").
-        named!(compressed<ExpzipToken>,
-            do_parse!(
-                char!('(') >>
-                len: number >>
-                char!('x') >>
-                count: number >>
-                char!(')') >>
-                to_repeat: take_str!(len) >>
-                (ExpzipToken::compressed(to_repeat.to_string(), count))
-            )
-        );
-
         // helper returning true as long as `x` is not the start of a compression marker.
         fn not_marker_start(x: u8) -> bool {
             x != '(' as u8
         }
 
         // parse an uncompressed chunk of data (i.e. "decompressed section").
-        named!(uncompressed<ExpzipToken>,
+        named!(uncompressed<EzipNode>,
             do_parse!(
                 data: map_res!(take_while!(not_marker_start), str::from_utf8) >>
-                (ExpzipToken::uncompressed(data.trim_right().to_string()))
+                (EzipNode::Uncompressed(data.trim_right().to_string()))
+            )
+        );
+
+        // parse a marker (eg. "(3x6)") and return a tuple with its two numbers (eg. `(3, 6)`).
+        named!(marker<(usize, usize)>,
+            do_parse!(
+                char!('(') >> len: number >> char!('x') >> count: number >> char!(')') >>
+                (len, count)
+            )
+        );
+
+        // parse a full marker (eg. "(3x6)") and return only the data length (eg. `3`).
+        named!(marker_len<usize>,
+            do_parse!(
+                char!('(') >> len: number >> char!('x') >> number >> char!(')') >>
+                (len)
+            )
+        );
+
+        // parse a compressed version 1 marker and its associated data, eg. "(3x6)XYZ".
+        named!(compressed_v1<EzipNode>,
+            do_parse!(
+                mark: marker >>
+                children: map!(take_str!(mark.0), Ezip::build_uncompressed) >>
+                (EzipNode::Compressed(mark.1, children))
+            )
+        );
+
+        // parse a compressed version 2 marker and its associated data, eg. "(3x6)XYZ".
+        named!(compressed_v2<EzipNode>,
+            do_parse!(
+                mark: peek!(marker) >> // peek! the marker so that length_value! can consume it.
+                children: map!(length_value!(marker_len, nodes_v2), Ezip::build) >>
+                (EzipNode::Compressed(mark.1, children))
             )
         );
 
         // parse a chain of compressed and uncompressed chunk.
-        named!(tokens<Vec<ExpzipToken>>, many1!(alt!(compressed | uncompressed)));
+        named!(nodes_v1<Vec<EzipNode>>, many1!(alt!(compressed_v1 | uncompressed)));
+        named!(nodes_v2<Vec<EzipNode>>, many1!(alt!(compressed_v2 | uncompressed)));
 
-        // parse a full Expzip file.
-        named!(parse_expzip<Expzip>, do_parse!(xs: tokens >> (Expzip { tokens: xs })));
+        // parse a full Ezip file.
+        named!(parse_ezipv1<Ezip>, map!(nodes_v1, Ezip::build));
+        named!(parse_ezipv2<Ezip>, map!(nodes_v2, Ezip::build));
 
-        impl FromStr for Expzip {
-            type Err = nom::IError;
+        // expose the ezipv1 parser outside this mod.
+        pub fn ezipv1(s: &str) -> nom::IResult<&[u8], Ezip> {
+            parse_ezipv1(s.as_bytes())
+        }
 
-            fn from_str(s: &str) -> Result<Expzip, Self::Err> {
-                parse_expzip(s.as_bytes()).to_full_result()
-            }
+        // expose the ezipv2 parser outside this mod.
+        pub fn ezipv2(s: &str) -> nom::IResult<&[u8], Ezip> {
+            parse_ezipv2(s.as_bytes())
         }
     }
 }
@@ -105,49 +150,92 @@ fn main() {
     let stdin = std::io::stdin();
     stdin.lock().read_to_string(&mut input).expect("no input given");
 
-    let compressed: Expzip = input.parse().unwrap();
-    println!("the decompressed length of the file is {}.", compressed.uncompressed_len());
+    // parse input as Ezip version 1
+    let compressed = Ezip::parse_v1(input.as_str()).unwrap();
+    println!("the decompressed length of the file (v1) is {}.", compressed.uncompressed_len());
+
+    // parse input as Ezip version 2
+    let compressed = Ezip::parse_v2(input.as_str()).unwrap();
+    println!("the decompressed length of the file (v2) is {}.", compressed.uncompressed_len());
 }
 
 
 #[test]
 fn part1_first_example() {
-    let compressed: Expzip = "ADVENT".parse().unwrap();
+    let s = "ADVENT";
+    let compressed = Ezip::parse_v1(s).unwrap();
     println!("{:?}", compressed);
     assert_eq!(compressed.uncompressed_len(), 6);
 }
 
 #[test]
 fn part1_second_example() {
-    let compressed: Expzip = "A(1x5)BC".parse().unwrap();
+    let s = "A(1x5)BC";
+    let compressed = Ezip::parse_v1(s).unwrap();
     println!("{:?}", compressed);
     assert_eq!(compressed.uncompressed_len(), 7);
 }
 
 #[test]
 fn part1_third_example() {
-    let compressed: Expzip = "(3x3)XYZ".parse().unwrap();
+    let s = "(3x3)XYZ";
+    let compressed = Ezip::parse_v1(s).unwrap();
     println!("{:?}", compressed);
     assert_eq!(compressed.uncompressed_len(), 9);
 }
 
 #[test]
 fn part1_fourth_example() {
-    let compressed: Expzip = "A(2x2)BCD(2x2)EFG".parse().unwrap();
+    let s = "A(2x2)BCD(2x2)EFG";
+    let compressed = Ezip::parse_v1(s).unwrap();
     println!("{:?}", compressed);
     assert_eq!(compressed.uncompressed_len(), 11);
 }
 
 #[test]
 fn part1_fifth_example() {
-    let compressed: Expzip = "(6x1)(1x3)A".parse().unwrap();
+    let s = "(6x1)(1x3)A";
+    let compressed = Ezip::parse_v1(s).unwrap();
     println!("{:?}", compressed);
     assert_eq!(compressed.uncompressed_len(), 6);
 }
 
 #[test]
 fn part1_sixth_example() {
-    let compressed: Expzip = "X(8x2)(3x3)ABCY".parse().unwrap();
+    let s = "X(8x2)(3x3)ABCY";
+    let compressed = Ezip::parse_v1(s).unwrap();
     println!("{:?}", compressed);
     assert_eq!(compressed.uncompressed_len(), 18);
+}
+
+#[test]
+fn part2_first_example() {
+    let s = "(3x3)XYZ";
+    let compressed = Ezip::parse_v2(s).unwrap();
+    println!("{:?}", compressed);
+    assert_eq!(compressed.uncompressed_len(), 9);
+}
+
+#[test]
+fn part2_second_example() {
+    let s = "X(8x2)(3x3)ABCY";
+    let compressed = Ezip::parse_v2(s).unwrap();
+    println!("{:?}", compressed);
+    assert_eq!(compressed.uncompressed_len(), 20);
+}
+
+#[test]
+fn part2_third_example() {
+    let s = "(27x12)(20x12)(13x14)(7x10)(1x12)A";
+    let compressed = Ezip::parse_v2(s).unwrap();
+    println!("{:?}", compressed);
+    assert_eq!(compressed.uncompressed_len(), 241920);
+}
+
+#[test]
+fn part2_fourth_example() {
+    let s = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN";
+    let compressed = Ezip::parse_v2(s).unwrap();
+    println!("{:?}", compressed);
+    assert_eq!(compressed.uncompressed_len(), 445);
 }
